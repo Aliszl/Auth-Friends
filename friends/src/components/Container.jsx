0d291ad2f7d1dfd5bfa38f90b0581dbf80c220d0
Alison Law <alisparklaw@gmail.com>
2019-12-17 11:57:06 +0000

    Create friends app- basic file set up

---
+++ friends/src/components/Container.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route, NavLink, withRouter, Redirect } from 'react-router-dom';
+import Login from './Login';
+import Quotes from './Quotes';
+import './Container.less';
