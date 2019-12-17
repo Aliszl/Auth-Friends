c8006a9c4376e5612dbe38fcdf80105594383ee9
Alison Law <alisparklaw@gmail.com>
2019-12-17 17:46:14 +0000

    add friend form

--- friends/src/components/Container.jsx
+++ friends/src/components/Container.jsx
@@ -1,75 +1,75 @@
-import React from 'react';
-import { Route, NavLink, withRouter, Redirect } from 'react-router-dom';
-import Login from './Login';
-import Friends from './Friends';
-
+import React from "react";
+import { Route, NavLink, withRouter, Redirect } from "react-router-dom";
+import Login from "./Login";
+import Friends from "./Friends";
+import addFriendForm from "./AddFriend";
 
 export function Container(props) {
-    const onLogout = () => {
-      // 1- We need to flush token from local storage
-      localStorage.removeItem('token');
-      // 2- We need to redirect users to login route props.history
-      props.history.push("/")
-    };
-  
-    return (
-      <div className='container'>
-        <nav>
-          <span>
-            <NavLink exact to='/'>Login</NavLink>
-            <NavLink to='/friends'>Friends</NavLink>
-          </span>
-  
-          <button onClick={onLogout}>Logout</button>
-        </nav>
-  
-        <main>
-          <Route
-            exact
-            path='/'
-            component={Login}
-          />
+  const onLogout = () => {
+    // 1- We need to flush token from local storage
+    localStorage.removeItem("token");
+    // 2- We need to redirect users to login route props.history
+    props.history.push("/");
+  };
 
-<PrivateRoute
-          path='/friends'
-          component={Friends}
-        />
-  
-         
-  
-          {/* (OPTION B) Create a secure Route for Quotes.
+  return (
+    <div className="container">
+      <nav>
+        <span>
+          <NavLink exact to="/">
+            Login
+          </NavLink>
+
+          <button onClick={onLogout}>Logout</button>
+          <br />
+          <br />
+          <NavLink to="/add_friend">Add Friend</NavLink>
+          <br />
+          <br />
+          <NavLink to="/friends">Friends</NavLink>
+          <br />
+          <br />
+        </span>
+      </nav>
+
+      <main>
+        <Route exact path="/" component={Login} />
+
+        <PrivateRoute path="/friends" component={Friends} />
+        <PrivateRoute path="/add_friend" component={addFriendForm} />
+
+        {/* (OPTION B) Create a secure Route for Quotes.
           Alternatively, we could have the Quotes component
           itself handle the redirect if no token. */}
-          {/* <Route
+        {/* <Route
             exact
-            path='/quotes'
-            render={props => withAuthCheck(Quotes, props)}
+            path='/add_friend'
+            render={props => withAuthCheck(addFriendForm, props)}
           /> */}
-        </main>
-      </div>
-    );
-  }
-  const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        localStorage.getItem("token") ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/" />
-        )
-      }
-    />
+      </main>
+    </div>
   );
-
-  function withAuthCheck(Component, props) {
-    // if token render component passing props to it
-    // otherwise use the Redirect to bounce you away
-    if (localStorage.getItem('token')) {
-      return <Component {...props} />
+}
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
     }
-    return <Redirect to='/' />
+  />
+);
+
+function withAuthCheck(Component, props) {
+  // if token render component passing props to it
+  // otherwise use the Redirect to bounce you away
+  if (localStorage.getItem("token")) {
+    return <Component {...props} />;
   }
-  
-  export default withRouter(Container);
-  
+  return <Redirect to="/" />;
+}
+
+export default withRouter(Container);
